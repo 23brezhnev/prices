20aede7442a22f388489521a98201f8a1c5425bb
Viktor Brezhnev <vitya.br@yandex.ru>
2025-03-03 13:52:21 +0300

    modified:   index.html - Добавлен интерфейс для авторизации и управления товарами с использованием Ant Design Vue

---
+++ config.example.js
@@ -0,0 +1,4 @@
+const SUPABASE_CONFIG = {
+    SUPABASE_URL: 'https://your-project-id.supabase.co',
+    SUPABASE_KEY: 'your-public-key'
+}; 
---
+++ config.js
@@ -0,0 +1,4 @@
+const SUPABASE_CONFIG = {
+    SUPABASE_URL: 'https://eeamyvvtzxbtdngdnofc.supabase.co',
+    SUPABASE_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVlYW15dnZ0enhidGRuZ2Rub2ZjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDA0MjAyMTUsImV4cCI6MjA1NTk5NjIxNX0.tjAJ0ZvyqOcJKI2P1bhXts4higZzemZskAx2OZ-P6_0'
+}; 
